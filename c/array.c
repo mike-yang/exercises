d0eabb212416971c957a2c7e8ec385d71ac78f4f
mike <qfpmike@hotmail.com>
2014-12-02 14:57:28 +0800

    histogram printed randomly

--- c/array.c
+++ c/array.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 20
+#define N 100
 
 int a[N];
 
 void gen_random(int upper_bound)
 {
 	int i;
+	srand(time(NULL));
 	for (i = 1; i < N; i++)
 		a[i] = rand() % upper_bound;
 }
 
 void print_random()
 {
-	int i;
-	for (i = 0; i < N; i++)
-		printf("%d ", a[i]);
+	int i, j, histogram[10] = {0};
+	for (i = 0; i < N; i++) 
+		histogram[a[i]]++;
+		for(j = 0; j < 10; j++) {
+		printf("%d  ", j);
+		for(i = 0; i < histogram[j]; i++)
+		printf("%c", '*');
 	printf("\n");
+	}
 }
 
 int main(void)
